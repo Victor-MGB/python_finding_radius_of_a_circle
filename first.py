8da91910e98470087ae1a9b2625c599c3a7cb329
Victor-MGB <mgbemenaosonduv@gmail.com>
2024-08-09 17:35:22 +0100

    python finding the radius of circle

---
+++ first.py
@@ -0,0 +1,12 @@
+import math
+
+radius = float(input("Enter the radius of the circle :"))
+area = math.pi * (radius ** 2)
+print(f"The area of the circle with radius {radius} is {area:.2f}")
+
+
+
+
+# # Step 1: Get the radius from the user
+# Step 2: Calculate the area using the formula A = πr^2
+# Step 3: Display the resul
